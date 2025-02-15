5b644ab8a82c3b45b6eaaffdcfe36fc997123d3c
Ibrahim Anifowoshe <ibrahim.anifowoshe.uk@gmail.com>
2025-02-15 19:27:54 +0000

    Update Playwright configuration to remove default baseURL; rely solely on environment variable

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   use: {
     browserName: "chromium",
     headless: true,
-    baseURL: process.env.BASE_URL || "http://localhost:3000",
+    baseURL: process.env.BASE_URL,
     trace: "on-first-retry",
     video: "on-first-retry",
     screenshot: "only-on-failure",
